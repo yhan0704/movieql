6f781f23c4cead119a3ab3ab2bb20c61ded89512
Young Han <hanyc1205@gmail.com>
2022-01-24 08:42:17 -0500

    third movie api to Graphql

--- graphql/db.js
+++ graphql/db.js
@@ -1,55 +1,69 @@
-export let movies = [
-  {
-    id: 1,
-    name: "Young Chan",
-    score: 1,
-  },
-  {
-    id: 2,
-    name: "Young In",
-    score: 8,
-  },
-  {
-    id: 3,
-    name: "Young Ji",
-    score: 99,
-  },
-  {
-    id: 4,
-    name: "Young A",
-    score: 2,
-  },
-];
+import axios from "axios";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id !== id);
-  return filteredMovies[0];
+export const getMovies = async (limit, rating) => {
+  const res = await axios.get(API_URL).then((res) => res.data.data.movies);
+  return res;
 };
 
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-  //   const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  //   return cleanedMovies;
-};
+// export const getMovies = (limit, rating) =>
+//   fetch(API_URL)
+//     .then((res) => res.json())
+//     .then((json) => json.data.movies);
 
-const findLastId = () => {
-  return movies[movies.length - 1].id;
-};
+// export let movies = [
+//   {
+//     id: 1,
+//     name: "Young Chan",
+//     score: 1,
+//   },
+//   {
+//     id: 2,
+//     name: "Young In",
+//     score: 8,
+//   },
+//   {
+//     id: 3,
+//     name: "Young Ji",
+//     score: 99,
+//   },
+//   {
+//     id: 4,
+//     name: "Young A",
+//     score: 2,
+//   },
+// ];
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: findLastId() + 1,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
+// export const getMovies = () => movies;
+
+// export const getById = (id) => {
+//   const filteredMovies = movies.filter((movie) => movie.id !== id);
+//   return filteredMovies[0];
+// };
+
+// export const deleteMovie = (id) => {
+//   const cleanedMovies = movies.filter((movie) => movie.id !== id);
+//   if (movies.length > cleanedMovies.length) {
+//     movies = cleanedMovies;
+//     return true;
+//   } else {
+//     return false;
+//   }
+//   //   const cleanedMovies = movies.filter((movie) => movie.id !== id);
+//   //   return cleanedMovies;
+// };
+
+// const findLastId = () => {
+//   return movies[movies.length - 1].id;
+// };
+
+// export const addMovie = (name, score) => {
+//   const newMovie = {
+//     id: findLastId() + 1,
+//     name,
+//     score,
+//   };
+//   movies.push(newMovie);
+//   return newMovie;
+// };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,13 +2,13 @@ import { getMovies, getById, deleteMovie, addMovie } from "./db";
 
 const resolvers = {
   Query: {
-    movies: getMovies,
+    movies: () => getMovies(),
     // person: (_, { id }) => getById(id),
   },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
-  },
+  // Mutation: {
+  //   addMovie: (_, { name, score }) => addMovie(name, score),
+  //   deleteMovie: (_, { id }) => deleteMovie(id),
+  // },
 };
 
 export default resolvers;
